d07940b3e15973f9d8a441657db5f46040be6d01
Jim Clark <jim.clark@generalassemb.ly>
2021-07-30 10:29:29 -0700

    Finish Infrastructure Part 2

--- src/index.js
+++ src/index.js
@@ -1,12 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { BrowserRouter as Router } from 'react-router-dom';
 import './index.css';
 import App from './pages/App/App';
 // import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Router><App /></Router>
   </React.StrictMode>,
   document.getElementById('root')
 );
--- src/pages/App/App.jsx
+++ src/pages/App/App.jsx
@@ -1,9 +1,32 @@
+import { useState } from 'react';
+import { Redirect, Route, Switch } from 'react-router-dom';
 import './App.css';
+import AuthPage from '../AuthPage/AuthPage';
+import NewOrderPage from '../NewOrderPage/NewOrderPage';
+import OrderHistoryPage from '../OrderHistoryPage/OrderHistoryPage';
+import NavBar from '../../components/NavBar/NavBar';
 
 export default function App() {
+  const [user, setUser] = useState({});
+
   return (
     <main className="App">
-      App
+      { user ?
+        <>
+          <NavBar />
+          <Switch>
+            <Route path="/orders/new">
+              <NewOrderPage />
+            </Route>
+            <Route path="/orders">
+              <OrderHistoryPage />
+            </Route>
+            <Redirect to="/orders" />
+          </Switch>
+        </>
+        :
+        <AuthPage />
+      }
     </main>
   );
 }
