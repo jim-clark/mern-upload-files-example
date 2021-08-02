3e5163ab04703026f3b21fe9b39202c0bfa85b04
Jim Clark <jim.clark@generalassemb.ly>
2021-08-02 14:28:31 -0700

    Finish Infrastructure Part 5

--- controllers/api/users.js
+++ controllers/api/users.js
@@ -1,13 +1,33 @@
+const jwt = require('jsonwebtoken');
+const User = require('../../models/user');
+
 module.exports = {
   create
 };
 
-function create(req, res) {
-  // TODO:  Baby step
-  res.json({
-    user: {
-      name: req.body.name,
-      email: req.body.email
-    }
-  });
+async function create(req, res) {
+  try {
+    // Add the user to the database
+    const user = await User.create(req.body);
+    // token will be a string
+    const token = createJWT(user);
+    // Yes, we can use res.json to send back just a string
+    // The client code take this into consideration
+    res.json(token);
+  } catch (err) {
+    // Client will check for non-2xx status code 
+    // 400 = Bad Request
+    res.status(400).json(err);
+  }
+}
+
+/*-- Helper Functions --*/
+
+function createJWT(user) {
+  return jwt.sign(
+    // data payload
+    { user },
+    process.env.SECRET,
+    { expiresIn: '24h' }
+  );
 }
--- crud-helper.js
+++ crud-helper.js
@@ -3,7 +3,7 @@ require('dotenv').config();
 require('./config/database');
 
 // Require the Mongoose models
-// const User = require('./models/user');
+const User = require('./models/user');
 // const Item = require('./models/item');
 // const Category = require('./models/category');
 // const Order = require('./models/order');
