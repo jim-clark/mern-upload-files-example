715758c89b2dbe74c34e25e6b9a00db783a343c3
Jim Clark <jim.clark@generalassemb.ly>
2021-08-03 13:58:20 -0700

    Finish Step 7 - mern-infrastructure is now complete

--- routes/api/users.js
+++ routes/api/users.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const router = express.Router();
 const usersCtrl = require('../../controllers/api/users');
+const ensureLoggedIn = require('../../config/ensureLoggedIn');
 
 // POST /api/users
 router.post('/', usersCtrl.create);
@@ -8,6 +9,6 @@ router.post('/', usersCtrl.create);
 router.post('/login', usersCtrl.login);
 
 // GET /api/users/check-token
-router.get('/check-token', usersCtrl.checkToken);
+router.get('/check-token', ensureLoggedIn, usersCtrl.checkToken);
 
 module.exports = router;
