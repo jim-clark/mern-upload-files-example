0b492da7ff9b87276a3a769a0bf244042a535dc5
Jim Clark <jim.clark@generalassemb.ly>
2023-07-19 07:58:08 -0700

    Add token code to send-request.js

--- src/utilities/send-request.js
+++ src/utilities/send-request.js
@@ -1,4 +1,6 @@
-// NOTE:  This version of sendRequest does not include the code for sending a token in the request
+// IMPORTANT:
+// Uncomment the below import if using MERN-Infrastructure & token-based auth
+// import { getToken } from './users-service';
 
 export default async function sendRequest(url, method = 'GET', payload = null, payloadIsFormData = false) {
   // Fetch accepts an options object as the 2nd argument
@@ -11,6 +13,13 @@ export default async function sendRequest(url, method = 'GET', payload = null, p
     // If payload is a FormData object, don't stringify it
     options.body = payloadIsFormData ? payload : JSON.stringify(payload);
   }
+  // IMPORTANT:
+  // Uncomment the code below if using MERN-Infrastructure & token-based auth
+  // const token = getToken();
+  // if (token) {
+  //   options.headers = options.headers || {};
+  //   options.headers.Authorization = `Bearer ${token}`;
+  // }
   const res = await fetch(url, options);
   // res.ok will be false if the status code set to 4xx in the controller action
   if (res.ok) return res.json();
